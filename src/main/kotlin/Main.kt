d583d0b3e515f0a1b62c91cac58eb2d17f8348f9
AndrewQJLIN <nut01@yandex.ru>
2023-04-21 17:27:13 +0400

    TB_2_1 дата класс и число выученных слов

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,19 @@
 import java.io.File
 
 fun main() {
+    val dictionary: MutableList<Word> = mutableListOf()
+
     val wordsFile: File = File("words.txt")
-    wordsFile.forEachLine { println(it) }
+    wordsFile.forEachLine {
+        val line = it.split("|")
+        dictionary += Word(original = line[0], translated = line[1], correctAnswersCount = line[2])
+    }
+    dictionary.forEach { println(it) }
 }
+
+data class Word(
+    val original: String,
+    val translated: String,
+    val correctAnswersCount: String? = null,
+)
+
