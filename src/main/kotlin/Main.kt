0c0ae0d1b138730dc5567d4d81abb586a05ffcd4
AndrewQJLIN <nut01@yandex.ru>
2023-04-22 14:49:48 +0400

    TB_2_1 изм. correctAnswersCount тип INT проверка на null

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,10 +3,10 @@ import java.io.File
 fun main() {
     val dictionary: MutableList<Word> = mutableListOf()
 
-    val wordsFile: File = File("words.txt")
+    val wordsFile = File("words.txt")
     wordsFile.forEachLine {
         val line = it.split("|")
-        dictionary += Word(original = line[0], translated = line[1], correctAnswersCount = line[2])
+        dictionary += Word(original = line[0], translated = line[1], correctAnswersCount = line[2].toIntOrNull() ?: 0)
     }
     dictionary.forEach { println(it) }
 }
@@ -14,6 +14,6 @@ fun main() {
 data class Word(
     val original: String,
     val translated: String,
-    val correctAnswersCount: String? = null,
+    val correctAnswersCount: Int = 0,
 )
 
