839a5de60ff28b4e9f920848d18d768157a209bb
AndrewQJLIN <nut01@yandex.ru>
2023-04-23 16:18:24 +0400

    TB_4_1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,30 +8,100 @@ fun main() {
         val line = it.split("|")
         dictionary += Word(original = line[0], translated = line[1], correctAnswersCount = line[2].toIntOrNull() ?: 0)
     }
+
+    if (!hasUnlearnedWords(dictionary)) return
     do {
         println("Меню: 1 – Учить слова, 2 – Статистика, 0 – Выход")
-        val input = readln()
-        var inputInt = 99
-        try {
-            inputInt = input.toInt()
-        } catch (_: NumberFormatException) {
-        }
-        when (inputInt) {
+        when (getAnswerNumber()) {
             0 -> break
-            1 -> println("Учим слова")
+            1 -> {
+                println("Для выхода из режима  - 0")
+                do {
+                    var sizeListToScreen = NUMBER_UNLEARNED_WORDS_SCREEN
+                    val unlearnedList =
+                        dictionary
+                            .shuffled()
+                            .filter { it.correctAnswersCount < NUMBER_CORRECTLY_LEARNED }.toMutableList()
+
+                    val sizeUnlearnedList = NUMBER_UNLEARNED_WORDS_SCREEN - unlearnedList.size
+                    if (sizeUnlearnedList > 0) {
+                        unlearnedList += if (dictionary.filter { it.correctAnswersCount >= NUMBER_CORRECTLY_LEARNED }.size >= sizeUnlearnedList) {
+                            dictionary.filter { it.correctAnswersCount >= NUMBER_CORRECTLY_LEARNED }
+                                .take(sizeUnlearnedList)
+                        } else {
+                            dictionary.filter { it.correctAnswersCount >= NUMBER_CORRECTLY_LEARNED }
+                        }
+                    }
+                    if (unlearnedList.size < NUMBER_UNLEARNED_WORDS_SCREEN) sizeListToScreen = unlearnedList.size
+
+                    unlearnedList.shuffled().take(sizeListToScreen).toMutableList()
+
+                    var indexWordRightAnswer: Int
+                    do {
+                        indexWordRightAnswer = (0 until sizeListToScreen).random()
+                        if (unlearnedList[indexWordRightAnswer].correctAnswersCount < NUMBER_CORRECTLY_LEARNED)
+                            break
+                    } while (true)
+
+                    println(unlearnedList[indexWordRightAnswer].original)
+                    for (index in 0 until sizeListToScreen - 1) {
+                        print("${index + 1} - ${unlearnedList[index].translated}, ")
+                    }
+                    println("$sizeListToScreen - ${unlearnedList[sizeListToScreen - 1].translated}")
+
+                    val inputAnswer = getAnswerNumber()
+                    if (inputAnswer == 0) break
+                    if (indexWordRightAnswer == (inputAnswer - 1)) {
+                        println("Правильно!")
+                        dictionary[dictionary.indexOfFirst { it == unlearnedList[indexWordRightAnswer] }].correctAnswersCount++
+                    } else {
+                        println("Неправильно - слово [ ${unlearnedList[indexWordRightAnswer].translated} ]")
+                    }
+                } while (hasUnlearnedWords(dictionary))
+            }
+
             2 -> {
-                val learnedWords = dictionary.filter { it.correctAnswersCount >= 3 }.size
+                val learnedWords = dictionary.filter { it.correctAnswersCount >= NUMBER_CORRECTLY_LEARNED }.size
                 val totalWords = dictionary.size
                 println("Выучено $learnedWords из $totalWords | ${((learnedWords * 100 / totalWords))}%")
             }
+
             else -> println("Введите только 1,2 или 0")
         }
     } while (true)
+
+    wordsFile.writeText("")
+    dictionary.forEach {
+        wordsFile.appendText("${it.original}|${it.translated}|${it.correctAnswersCount}\n")
+    }
 }
 
 data class Word(
     val original: String,
     val translated: String,
-    val correctAnswersCount: Int = 0,
+    var correctAnswersCount: Int = 0,
 )
+
+const val NUMBER_CORRECTLY_LEARNED = 3
+const val NUMBER_UNLEARNED_WORDS_SCREEN = 4
+
+fun hasUnlearnedWords(dictionary: MutableList<Word>): Boolean {
+    return if (dictionary.count { it.correctAnswersCount < NUMBER_CORRECTLY_LEARNED } > 0) {
+        true
+    } else {
+        println("Вы выучили все слова!")
+        false
+    }
+}
+
+fun getAnswerNumber(): Int {
+    val input = readln()
+    var inputInt = 99
+    return try {
+        inputInt = input.toInt()
+        inputInt
+    } catch (_: NumberFormatException) {
+        inputInt
+    }
+}
 
